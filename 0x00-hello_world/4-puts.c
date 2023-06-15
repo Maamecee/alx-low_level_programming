bba66f5a0f16afd3166da029d29a29a4094921e0
Agnes Otiwaa <maameceeotiwaa@gmial.com>
2023-06-15 07:48:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry point
+ * main - print "Programming is like building a multilingual puzzle, followed by a newline."
  *
- * Return: Always 0 (Success)
+ * return 0.
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-		return (0);
+
+	return(0);
 }
