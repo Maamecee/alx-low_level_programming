708df76d0df8084d28eb9ac5fac1ceef9c21b5ed
Agnes Otiwaa <maameceeotiwaa@gmial.com>
2023-06-21 06:39:54 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
  *    separated by a comma followed by a space.
@@ -10,6 +11,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+
 
 	for (count = 0; count < 50; count++)
 	{
